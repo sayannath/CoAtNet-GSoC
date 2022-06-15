ac53d407bad42603aa932d27152f20afedabb870
Sayan Nath <sayannath235@gmail.com>
2022-06-15 14:45:09 +0530

    Revised Feedback Round I

--- coatnet/models/coatnet.py
+++ coatnet/models/coatnet.py
@@ -5,26 +5,21 @@ from tensorflow import keras
 from coatnet.models.blocks import conv_3x3
 
 
-class CoAtNet(keras.models.Model):
-    def __init__(self, model_name: str, image_shape: tuple, num_classes: int, **kwargs):
-        super(CoAtNet, self).__init__(**kwargs)
-        self.model_name = model_name
-        self.image_shape = image_shape
-        self.num_classes = num_classes
+def get_training_model(
+    model_name: str, image_shape: tuple, num_classes: int
+) -> keras.Model:
+    """
+    Implements CoAtNet family of models given a configuration.
+    References:
+        (1) https://arxiv.org/pdf/2106.04803.pdf
+    """
+    configs = get_model_config(model_name)
 
-    def get_training_model(self):
-        """
-        Implements CoAtNet family of models given a configuration.
-        References:
-            (1) https://arxiv.org/pdf/2106.04803.pdf
-        """
-        configs = get_model_config(self.model_name)
-
-        input_layer = keras.Input(shape=self.image_shape)  # Input Layer
-        conv_1 = conv_3x3(
-            input_layer=input_layer,
-            num_filters=configs.out_channels[0],
-            downsample=True,
-        )  # Conv Stem Block
-        conv_2 = conv_3x3(input_layer=conv_1, num_filters=configs.out_channels[0])
-        print(conv_2.shape)
+    input_layer = keras.Input(shape=image_shape)  # Input Layer
+    conv_1 = conv_3x3(
+        input_layer=input_layer,
+        num_filters=configs.out_channels[0],
+        downsample=True,
+    )  # Conv Stem Block
+    conv_2 = conv_3x3(input_layer=conv_1, num_filters=configs.out_channels[0])
+    print(conv_2.shape)
--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from absl import app, flags, logging
 from ml_collections.config_flags import config_flags
 
-from coatnet.models.coatnet import CoAtNet
+from coatnet.models.coatnet import get_training_model
 
 FLAGS = flags.FLAGS
 config_flags.DEFINE_config_file("experiment_configs")
@@ -13,7 +13,7 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 def main(_):
 
-    model = CoAtNet(
+    model = get_training_model(
         model_name=FLAGS.experiment_configs.model_name,
         image_shape=(
             FLAGS.experiment_configs.image_height,
@@ -22,8 +22,6 @@ def main(_):
         ),
         num_classes=FLAGS.experiment_configs.num_classes,
     )
-
-    model.get_training_model()
 
 
 if __name__ == "__main__":
