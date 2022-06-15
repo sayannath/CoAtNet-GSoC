6ad4603a0a1e6c55fa041c40f9c8ca282164cf97
Sayan Nath <sayannath235@gmail.com>
2022-06-15 14:27:38 +0530

    Feedback Round I

--- coatnet/models/blocks.py
+++ coatnet/models/blocks.py
@@ -1,9 +1,16 @@
 import tensorflow as tf
-import tensorflow_addons as tfa
 from tensorflow.keras import layers
 
 
-def conv_3x3(input_layer, num_filters=64, downsample=False):
+def conv_3x3(input_layer, num_filters: int = 64, downsample: bool = False):
+    """
+    3x3 Convolutional Stem Stage.
+
+    Args:
+        input_layer: input tensor
+        num_filters: number of filters in the convolutional layer
+        downsample: whether to downsample the input layer or not
+    """
     stride = 1 if downsample == False else 2
     conv_1 = layers.Conv2D(
         filters=num_filters,
@@ -13,5 +20,5 @@ def conv_3x3(input_layer, num_filters=64, downsample=False):
         use_bias=False,
     )(input_layer)
     bn_1 = layers.BatchNormalization()(conv_1)
-    act_1 = tfa.layers.GELU()(bn_1)
+    act_1 = tf.nn.gelu(bn_1)
     return act_1
